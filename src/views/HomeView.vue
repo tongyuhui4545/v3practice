62fcc84f9c0ffed37543837220b9c9b1ac8fb18e
TongYuhui <tongyuhui@corp.netease.com>
2023-01-29 19:47:52 +0800

    just started

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+   <div class="home">
+    <div class="container">
+        <!-- Reviews Form -->
+        <!-- Reviews Stats -->
+        <!-- Reviews List -->
+    </div>
+   </div>
+</template>
+
+<script setup>
+ 
+</script>
