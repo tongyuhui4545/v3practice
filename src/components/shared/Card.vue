7fe7da5cba77631f7ba3030c37cf4e669f1390ed
TongYuhui <tongyuhui@corp.netease.com>
2023-01-29 20:04:25 +0800

    changes

---
+++ src/components/shared/Card.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card" :class="{ reverse: reverse }">
+    <slot />
+  </div>
+</template>
+>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    reverse: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
